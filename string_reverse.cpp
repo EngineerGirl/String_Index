0c90fa4ca9693c35d55bdab365f7332ef79838ea
Sid <siddhata.s.patil@gmail.com>
2016-07-09 20:09:15 -0700

    Create string_reverse.cpp

---
+++ string_reverse.cpp
@@ -0,0 +1,45 @@
+/*
+ ==============================================================================================================================
+ Name        : string_length.cpp
+ Author      : Siddhata Patil
+ Copyright   : Copyright ©  Siddhata Patil. Sourcecode rights reserved.
+ Question    : Implement reverse of a string
+ ==============================================================================================================================
+ */
+ 
+#include <iostream>
+using namespace std;
+#include <string>
+
+int length(string s)
+{
+    int i;
+    for( i=0; s[i]!='\0';++i)
+    {
+
+    }
+    return i;
+}
+
+string reverse1 (string s)
+{
+    int i;
+    string temp="";
+    for(i=0; i<(length(s)/2);i++)
+    {
+          temp[i]=s[i];
+    s[i]=s[length(s)-1-i];
+    s[length(s)-1-i]=temp[i];
+    }
+
+    return s;
+}
+
+int main()
+{
+    char name[]="Siddhata";
+    string reverse_output;
+    reverse_output=reverse1 (name);
+    cout<<reverse_output;
+    return 0;
+}
